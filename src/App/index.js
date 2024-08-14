5347659f89c0f7927889fc2c61acef4af027fff0
GregBonhomme <gregoire.bonhomme@gmail.com>
2024-08-14 06:19:48 +0200

    correction du build pour un déploiement sur github

--- src/App/index.js
+++ src/App/index.js
@@ -1,6 +1,6 @@
 import { Provider } from 'react-redux'
 import { store } from './store';
-import { BrowserRouter as Router, Routes , Route } from 'react-router-dom';
+import { HashRouter as Router, Routes , Route } from 'react-router-dom';
 import Home from '../pages/Home';
 import Employees from '../pages/Employees';
 import "bootstrap/dist/js/bootstrap.bundle.min.js";
